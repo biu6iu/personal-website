19a21c02637bf00bfa307372cdb16050840e36b2
Steven <stevenlu@student.unimelb.edu.au>
2026-08-05 18:13:30 +1000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   publicDir: '../public',
-  base: '/personal-website/',
+  base: '/biu-biu/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
