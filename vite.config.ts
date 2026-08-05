ac16761a19a990d8ccb6f8e475218d6f1cee4bb4
Steven <stevenlu@student.unimelb.edu.au>
2026-08-05 18:05:00 +1000

    update domain root

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   publicDir: '../public',
-  base: '/',
+  base: '/personal-website/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
